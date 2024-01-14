5f62d5e616aa22fcd0e1f34fefd5fa6508e8ad27
RadityaKhairulAmri <tlauncher4646@gmail.com>
2024-01-14 17:55:01 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,20 @@
+<div class="w-full bg-develobe-900 h-[320px] text-white flex flex-row pt-12">
+  <div class="w-6/12 pl-28 flex flex-col">
+    <span class="font-quicksand text-4xl font-bold">MOVIES</span>
+    <span class="font-inter text-lg mt-4">Kami menyediakan rate review film dan TV Series<br>yang bisa jadi rekomendasi tontonan kamu<br>#BetterMovieEnjoyViewing</span>
+    <span class="font-inter text-lg mt-4">&copy; 2024 MOVIES</span>
+  </div>
+
+  <div class="w-3/12 flex flex-col">
+    <span class="font-inter font-bold text-lg">Website</span>
+    <a href="/" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Home</a>
+    <a href="/movies" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Movies</a>
+    <a href="/tv-shows" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">TV Shows</a>
+  </div>
+
+  <div class="w-3/12 flex flex-col">
+    <span class="font-inter font-bold text-lg">Social Media</span>
+    <a href="https://www.instagram.com/rya_khairul?igsh=MTNib2g1OTM0OWlidg==" target="_blank" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Instagram</a>
+    <a href="https://discord.gg/fUFqbePe" target="_blank" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Discord</a>
+  </div>
+</div>
